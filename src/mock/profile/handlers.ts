4fbdc3add19b71d7d4c1bf0a367726a6bb794248
Gountzjs <tomasjsdev@gmail.com>
2026-01-27 18:08:36 -0300

    feat: :sparkles: add mocking profiles and tags

--- src/mock/profile/handlers.ts
+++ src/mock/profile/handlers.ts
@@ -2,17 +2,25 @@ import { http, HttpResponse } from 'msw';
 
 export const handlers = [
   // Get Profile
-  http.get('http://localhost:4200/api/profiles/:username', () => {
-    return HttpResponse.json({}, { status: 200 });
+  http.get('http://localhost:4200/api/profiles/:username', async () => {
+    const response = await fetch('http://localhost:4200/data/profiles.json');
+    const profile = await response.json();
+    return HttpResponse.json(profile, { status: 200 });
   }),
 
   // Follow User
-  http.post('http://localhost:4200/api/profiles/:username/follow', () => {
-    return HttpResponse.json({}, { status: 200 });
+  http.post('http://localhost:4200/api/profiles/:username/follow', async () => {
+    const response = await fetch('http://localhost:4200/data/profiles.json');
+    const profile = await response.json();
+    profile.profile.following = true;
+    return HttpResponse.json(profile, { status: 200 });
   }),
 
   // Unfollow User
-  http.delete('http://localhost:4200/api/profiles/:username/follow', () => {
-    return HttpResponse.json({}, { status: 200 });
+  http.delete('http://localhost:4200/api/profiles/:username/follow', async () => {
+    const response = await fetch('http://localhost:4200/data/profiles.json');
+    const profile = await response.json();
+    profile.profile.following = false;
+    return HttpResponse.json(profile, { status: 200 });
   }),
 ];
--- src/mock/tags/handlers.ts
+++ src/mock/tags/handlers.ts
@@ -2,7 +2,9 @@ import { http, HttpResponse } from 'msw';
 
 export const handlers = [
   // Get Tags
-  http.get('http://localhost:4200/api/tags', () => {
-    return HttpResponse.json({}, { status: 200 });
+  http.get('http://localhost:4200/api/tags', async () => {
+    const response = await fetch('http://localhost:4200/data/tags.json');
+    const tags = await response.json();
+    return HttpResponse.json(tags, { status: 200 });
   }),
 ];
