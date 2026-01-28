f8c51e7e3586b77f23ba06e7ff8450f85d54a17c
Gountzjs <tomasjsdev@gmail.com>
2026-01-27 21:05:09 -0300

    feat: :sparkles: navigation integrate baselayout home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@/layouts/base-layout/base-layout').then((m) => m.BaseLayout),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@/pages/home/home').then((m) => m.Home),
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
+import { User } from '@/modules/common/services/user/user';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
+  providers: [User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
