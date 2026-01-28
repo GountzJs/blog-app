0e6ef95059685c1fa7c81cbb16a677603d04a00b
Gountzjs <tomasjsdev@gmail.com>
2026-01-28 12:40:43 -0300

    feat: :sparkles: pages home add button remove chip

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,19 +1,21 @@
 import { ListArticles } from '@/modules/articles/components/list-articles/list-articles';
 import { ListTags } from '@/modules/articles/components/list-tags/list-tags';
+import { Icons } from '@/modules/common/components/icons/icons';
 import { SubManager } from '@/modules/common/services/sub-manager/sub-manager';
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ListTags, ListArticles],
+  imports: [ListTags, ListArticles, Icons],
   templateUrl: './home.html',
   styleUrl: './home.css',
   providers: [SubManager],
 })
 export class Home implements OnInit, OnDestroy {
-  private readonly router = inject(ActivatedRoute);
+  private readonly aRoute = inject(ActivatedRoute);
   private readonly subManager = inject(SubManager);
+  private readonly router = inject(Router);
   tag = signal<string | undefined>(undefined);
 
   ngOnInit(): void {
@@ -25,10 +27,14 @@ export class Home implements OnInit, OnDestroy {
   }
 
   getParams(): void {
-    const sub = this.router.queryParams.subscribe((params) => {
+    const sub = this.aRoute.queryParams.subscribe((params) => {
       const tag = params['tag'] || undefined;
       this.tag.set(tag);
     });
     this.subManager.add(sub, 'get-params');
+  }
+
+  clearTag(): void {
+    this.router.navigate(['/']);
   }
 }
