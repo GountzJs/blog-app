5cc832c352039d2cc38ff100fa47dd764594aba7
Gountzjs <tomasjsdev@gmail.com>
2026-01-28 03:03:40 -0300

    refactor: :sparkles: modules articles services move tags into folder

--- src/app/modules/articles/services/tags/tags.ts
+++ src/app/modules/articles/services/tags/tags.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { environment } from '@env/environment';
 import { Observable } from 'rxjs';
-import { Tag } from '../models/entities/tag.entity';
+import { Tag } from '../../models/entities/tag.entity';
 
 @Injectable()
 export class Tags {
