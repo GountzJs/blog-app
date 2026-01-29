ad6c312186a9aca50c259b8b62a1c2b280ef0e51
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 01:21:34 -0300

    feat: :sparkles: modules articles services articles-feed integrate httpclient

--- src/app/modules/articles/services/articles-feed/articles-feed.ts
+++ src/app/modules/articles/services/articles-feed/articles-feed.ts
@@ -1,4 +1,28 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '@env/environment';
+import { Observable } from 'rxjs';
+import { GetAllArticlesFeedRequestDTO } from '../../models/dtos/get-all-articles-feed-request.dto';
+import { Article } from '../../models/entities/article.entity';
 
 @Injectable()
-export class ArticlesFeed {}
+export class ArticlesFeed {
+  private readonly httpClient = inject(HttpClient);
+  private readonly apiUrl = environment.apiUrl;
+
+  getAll({ limit, offset }: GetAllArticlesFeedRequestDTO): Observable<{
+    articles: Article[];
+    articlesCount: number;
+  }> {
+    const params: Record<string, string | number | boolean> = {};
+    if (limit) params['limit'] = limit;
+    if (offset) params['offset'] = offset;
+
+    return this.httpClient.get<{ articles: Article[]; articlesCount: number }>(
+      `${this.apiUrl}/articles/feed`,
+      {
+        params,
+      },
+    );
+  }
+}
