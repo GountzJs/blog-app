60ea985876eea598e119e6ad2cb0a49790d6cce2
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 04:29:55 -0300

    feat: :sparkles: modules user components btn-profile integrate img and isactive navigate

--- src/app/modules/user/components/btn-profile/btn-profile.ts
+++ src/app/modules/user/components/btn-profile/btn-profile.ts
@@ -2,12 +2,13 @@ import { Session } from '@/core/services/session';
 import { SubManager } from '@/modules/common/services/sub-manager/sub-manager';
 import { UserStore } from '@/modules/user/services/user-store/user-store';
 import { User } from '@/modules/user/services/user/user';
+import { Location, NgClass } from '@angular/common';
 import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-btn-profile',
-  imports: [RouterLink],
+  imports: [RouterLink, NgClass],
   templateUrl: './btn-profile.html',
   styleUrl: './btn-profile.css',
   providers: [SubManager, User],
@@ -17,6 +18,7 @@ export class BtnProfile implements OnInit, OnDestroy {
   private readonly userService = inject(User);
   private readonly subManager = inject(SubManager);
   private readonly session = inject(Session);
+  private readonly location = inject(Location);
 
   ngOnInit(): void {
     this.getUser();
@@ -43,5 +45,9 @@ export class BtnProfile implements OnInit, OnDestroy {
 
   get user() {
     return this.userStore.get();
+  }
+
+  isActive(): boolean {
+    return this.location.path().includes(`/profile/@${this.user?.username}`);
   }
 }
