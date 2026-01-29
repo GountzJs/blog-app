016f546c7e76b8f91ab259f168c688839d979b52
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 04:21:09 -0300

    feat: :sparkles: base-layout integrate btnprofile and isauth session

--- src/app/layouts/base-layout/base-layout.ts
+++ src/app/layouts/base-layout/base-layout.ts
@@ -1,18 +1,25 @@
+import { Session } from '@/core/services/session';
 import { Icons } from '@/modules/common/components/icons/icons';
+import { BtnProfile } from '@/modules/user/components/btn-profile/btn-profile';
 import { Location } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-base-layout',
-  imports: [RouterLink, RouterOutlet, Icons],
+  imports: [RouterLink, RouterOutlet, Icons, BtnProfile],
   templateUrl: './base-layout.html',
   styleUrl: './base-layout.css',
 })
 export class BaseLayout {
   private readonly location = inject(Location);
+  private readonly session = inject(Session);
 
   isActive(url: string) {
     return this.location.path().split('?')[0] === url;
+  }
+
+  get isAuth(): boolean {
+    return this.session.isAuth();
   }
 }
