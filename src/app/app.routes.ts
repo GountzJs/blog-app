ad0c5458f8e393a79508810cac421b67155991d4
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 02:07:22 -0300

    feat: :sparkles: approutes integrate guards and providers userstore

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,8 @@
+import { isAuthenticateGuard } from '@/core/guards/is-autenticate/is-authenticate-guard';
 import { TagStore } from '@/modules/articles/services/tag-store/tag-store';
 import { Routes } from '@angular/router';
+import { isNotAuthenticateGuard } from './core/guards/is-not-authenticate/is-not-authenticate-guard';
+import { UserStore } from './modules/common/services/user-store/user-store';
 
 export const routes: Routes = [
   {
@@ -19,6 +22,7 @@ export const routes: Routes = [
           {
             path: 'feed',
             loadComponent: () => import('@/pages/your-feed/your-feed').then((m) => m.YourFeed),
+            canActivate: [isAuthenticateGuard],
           },
         ],
         providers: [TagStore],
@@ -26,10 +30,12 @@ export const routes: Routes = [
       {
         path: 'auth/sign-in',
         loadComponent: () => import('@/pages/auth/login/login').then((m) => m.Login),
+        canActivate: [isNotAuthenticateGuard],
       },
       {
         path: 'auth/sign-up',
         loadComponent: () => import('@/pages/auth/register/register').then((m) => m.Register),
+        canActivate: [isNotAuthenticateGuard],
       },
       {
         path: 'docs',
@@ -40,5 +46,6 @@ export const routes: Routes = [
         redirectTo: '',
       },
     ],
+    providers: [UserStore],
   },
 ];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,9 @@
-import { User } from '@/modules/common/services/user/user';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  providers: [User],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
