dc544eb4d3079352f9499a78de0d5835bc564774
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 04:46:39 -0300

    feat: :sparkles: pages articles view-article create

---
+++ src/app/pages/articles/view-article/view-article.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-article',
+  imports: [],
+  templateUrl: './view-article.html',
+  styleUrl: './view-article.css',
+})
+export class ViewArticle {
+
+}
