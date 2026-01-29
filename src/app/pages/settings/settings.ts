48a45732fdc3ff68e17264c9e5066d400978b47b
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 08:16:23 -0300

    feat: :sparkles: pages settings create feature

--- src/app/pages/settings/settings.ts
+++ src/app/pages/settings/settings.ts
@@ -1,8 +1,10 @@
+import { BtnLogout } from '@/modules/user/components/btn-logout/btn-logout';
+import { FormUser } from '@/modules/user/components/form-user/form-user';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-settings',
-  imports: [],
+  imports: [BtnLogout, FormUser],
   templateUrl: './settings.html',
   styleUrl: './settings.css',
 })
