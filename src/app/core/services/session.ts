71051110be2040893d4e87b03f965f6642880785
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 03:17:31 -0300

    feat: :sparkles: core services create session

---
+++ src/app/core/services/session.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class Session {
+  set(token: string): void {
+    sessionStorage.setItem('token', token);
+  }
+
+  get(): string | null {
+    return sessionStorage.getItem('token');
+  }
+
+  clear(): void {
+    sessionStorage.removeItem('token');
+  }
+
+  isAuth(): boolean {
+    return !!this.get();
+  }
+}
