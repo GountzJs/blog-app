b217d1d8c4167ec0111943f22566f43b5cbb6b62
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 06:44:40 -0300

    feat: :sparkles: pages create article integrate form

--- src/app/pages/articles/create-article/create-article.ts
+++ src/app/pages/articles/create-article/create-article.ts
@@ -1,8 +1,9 @@
+import { FormCreate } from '@/modules/articles/components/form-create/form-create';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-create-article',
-  imports: [],
+  imports: [FormCreate],
   templateUrl: './create-article.html',
   styleUrl: './create-article.css',
 })
