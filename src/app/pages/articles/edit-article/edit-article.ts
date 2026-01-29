033dd8188f4680df26c388e5fd71f234fca419fa
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 04:46:55 -0300

    feat: :sparkles: pages articles edit-article create

---
+++ src/app/pages/articles/edit-article/edit-article.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-article',
+  imports: [],
+  templateUrl: './edit-article.html',
+  styleUrl: './edit-article.css',
+})
+export class EditArticle {}
