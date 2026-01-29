7fbb32b3ef03ca6063f8189291b2dca0fa7de5b8
Gountzjs <tomasjsdev@gmail.com>
2026-01-29 01:24:11 -0300

    feat: :sparkles: pages global-feed change name infinitescroll

--- src/app/pages/global-feed/global-feed.ts
+++ src/app/pages/global-feed/global-feed.ts
@@ -1,12 +1,12 @@
 import { ListArticles } from '@/modules/articles/components/list-articles/list-articles';
 import { TagStore } from '@/modules/articles/services/tag-store/tag-store';
-import { InfiniteScrollComponent } from '@/modules/common/components/infinite-scroll/infinite-scroll.component';
+import { InfiniteScroll } from '@/modules/common/components/infinite-scroll/infinite-scroll.component';
 import { SubManager } from '@/modules/common/services/sub-manager/sub-manager';
 import { Component, inject, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-global-feed',
-  imports: [ListArticles, InfiniteScrollComponent],
+  imports: [ListArticles, InfiniteScroll],
   templateUrl: './global-feed.html',
   styleUrl: './global-feed.css',
   providers: [SubManager],
